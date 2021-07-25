84a9399945e4860d36937f77506ebffe98b8450c
Marius Gavrilescu <marius@ieval.ro>
2021-07-25 20:14:59 +0300

    Changes for STM32, not split

--- keyboards/typetheory/halconf.h
+++ keyboards/typetheory/halconf.h
@@ -3,7 +3,7 @@ Copyright 2021 Marius Gavrilescu <marius@ieval.ro>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
+the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
@@ -15,9 +15,11 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT      16
-#define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR  4095
-#define TRANSIENT_EEPROM_SIZE           4096
-#define VIAL_KEYBOARD_UID {0xC7, 0xA0, 0xC4, 0x86, 0xCC, 0x3F, 0x8D, 0xFC}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
+#pragma once
+
+#define PAL_USE_CALLBACKS true
+#define PAL_USE_WAIT      true
+
+#define SERIAL_USB_BUFFERS_SIZE 256
+
+#include_next "halconf.h"
