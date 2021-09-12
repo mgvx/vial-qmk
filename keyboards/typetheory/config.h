cbd561b4fe071be6a4553650198f67cbcaeddba1
Marius Gavrilescu <marius@ieval.ro>
2021-09-12 19:23:43 +0300

    Pin assignment

--- keyboards/typetheory/config.h
+++ keyboards/typetheory/config.h
@@ -21,15 +21,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x5474
-#define DEVICE_VER      0x0002
+#define DEVICE_VER      0x0007
 #define MANUFACTURER    TypeTheory
 #define PRODUCT         Keyboard
 
 #define MATRIX_ROWS 10
 #define MATRIX_COLS 8
 
-#define MATRIX_COL_PINS { B0, B1, B10, B5, B4, B3, B7, B6 }
-#define MATRIX_ROW_PINS { A7, A6, A5, A4, A3 }
+#define MATRIX_COL_PINS { B8, B6, B7, B5, B4, B3, A6, A7 }
+#define MATRIX_ROW_PINS { A5, A4, A3, C15, A1 }
+#define MATRIX_COL_PINS_RIGHT { A1, C15, A3, A4, A5, A6, B9, B8 }
+#define MATRIX_ROW_PINS_RIGHT { B3, B4, B5, B6, B7 }
 
 #define SOFT_SERIAL_PIN A2
 #define SERIAL_USART_DRIVER SD2
